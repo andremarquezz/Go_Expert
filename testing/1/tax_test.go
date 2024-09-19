be579abca285caa062b4e21640741e23bbc8bf8b
Andre Marquez <andre.marqquez@gmail.com>
2024-09-19 11:37:13 -0300

    testing with if and table

--- testing/1/tax_test.go
+++ testing/1/tax_test.go
@@ -2,10 +2,27 @@ package tax
 
 import "testing"
 
-func TestCalculateTax(t *testing.T) {
-	expected := 2.0
-	result := CalculateTax(50.0)
-	if result != expected {
-		t.Errorf("Expected %f, got %f", expected, result)
+// func TestCalculateTax(t *testing.T) {
+// 	expected := 2.0
+// 	result := CalculateTax(50.0)
+// 	if result != expected {
+// 		t.Errorf("Expected %f, got %f", expected, result)
+// 	}
+// }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type test struct {
+		amount, expected float64
+	}
+	table := []test{
+		{50.0, 2.0},
+		{150.0, 3.0},
+		{-50.0, 0.0},
+	}
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expected {
+			t.Errorf("Expected %f, got %f", item.expected, result)
+		}
 	}
 }
