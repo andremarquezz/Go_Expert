6e61249d44587bb25f68209f0ac6045cf92f68f2
Andre Marquez <andre.marqquez@gmail.com>
2024-09-23 14:14:35 -0300

    entity user

--- APIs/configs/config.go
+++ APIs/configs/config.go
@@ -5,8 +5,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfg *conf
-
 type conf struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
 	DBHost        string `mapstructure:"DB_HOST"`
@@ -21,17 +19,21 @@ type conf struct {
 }
 
 func LoadConfig(path string) (*conf, error) {
+	var cfg *conf
+
 	viper.SetConfigName("app_config")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
+
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
 	if err := viper.Unmarshal(&cfg); err != nil {
 		panic(err)
 	}
+
 	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecretKey), nil)
 
 	return cfg, nil
---
+++ APIs/internal/entity/user_test.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "password")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, user.Name, "John Doe")
+	assert.Equal(t, user.Email, "j@j.com")
+}
