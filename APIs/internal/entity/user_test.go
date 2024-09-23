af78d3875cd8c6adef34c09c5802d33cb9db4fe1
Andre Marquez <andre.marqquez@gmail.com>
2024-09-23 14:19:15 -0300

    add: tests entity user

--- APIs/internal/entity/user_test.go
+++ APIs/internal/entity/user_test.go
@@ -15,3 +15,11 @@ func TestNewUser(t *testing.T) {
 	assert.Equal(t, user.Name, "John Doe")
 	assert.Equal(t, user.Email, "j@j.com")
 }
+
+func TestUserValidatePassword(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "123456")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123456"))
+	assert.False(t, user.ValidatePassword("1234567"))
+	assert.NotEqual(t, "123456", user.Password)
+}
